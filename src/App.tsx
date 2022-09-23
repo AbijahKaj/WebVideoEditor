5ad45ab0fb1115b59167dbf8a703d5c2ef80eada
Abijah Migisho <Abijah.Migisho@noveogroup.com>
2022-09-24 00:39:26 +0300

    feat: add file upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+import { FileDrop } from 'react-file-drop'
+
+
 const ffmpeg = createFFmpeg({ corePath: 'https://unpkg.com/@ffmpeg/core@0.10.0/dist/ffmpeg-core.js', log: true });
 
 function App() {
@@ -12,24 +14,47 @@ function App() {
     await ffmpeg.load();
     setReady(true);
   }
-
   useEffect(() => {
     load();
+    document.addEventListener('drop', function (e) {
+      e.preventDefault()
+      e.stopPropagation()
+    })
   }, [])
+
+  const startUploading = (files: FileList | null) => {
+    files && setVideo(files[0]);
+  }
 
 
   return ready ? (<div className="App">
-
-    {video && <video
-      controls
-      width="250"
-      src={URL.createObjectURL(video)}>
-
-    </video>}
-
-    <input type="file" onChange={(e) => setVideo(e.target?.files?.item(0)!)} />
-
-
+    <div className={'wrapper'}>
+      {video ? (
+        <div
+          style={{
+            maxHeight: '100vh',
+          }}> <video
+            controls={false}
+          >
+            <source src={URL.createObjectURL(video)}></source>
+          </video></div>
+      ) : (
+        <>
+          <input
+            onChange={(e) => startUploading(e.target.files)}
+            type='file'
+            className='hidden'
+            id='upload'
+          />
+          <FileDrop
+            onDrop={startUploading}
+            onTargetClick={() => document.getElementById('upload')?.click()}
+          >
+            Upload
+          </FileDrop>
+        </>
+      )}
+    </div>
   </div>) : (<p>Loading...</p >);
 }
 
