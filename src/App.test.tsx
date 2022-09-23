acecc531d79f8c60a996e9f9b48a30db9f4bf1a6
Abijah Migisho <Abijah.Migisho@noveogroup.com>
2022-09-23 23:47:00 +0300

    build: switch from snowpack

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,9 @@
-import * as React from 'react';
-import { render } from '@testing-library/react';
-import { expect } from 'chai';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-describe('<App>', () => {
-  it('renders learn react link', () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/learn react/i);
-    expect(document.body.contains(linkElement));
-  });
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-interface AppProps {}
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+const ffmpeg = createFFmpeg({ corePath: 'https://unpkg.com/@ffmpeg/core@0.10.0/dist/ffmpeg-core.js', log: true });
 
-function App({}: AppProps) {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
+function App() {
+  const [ready, setReady] = useState(false);
+  const [video, setVideo] = useState<MediaSource | File>();
+
+  const load = async () => {
+    await ffmpeg.load();
+    setReady(true);
+  }
+
   useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+    load();
+  }, [])
+
+
+  return ready ? (<div className="App">
+
+    {video && <video
+      controls
+      width="250"
+      src={URL.createObjectURL(video)}>
+
+    </video>}
+
+    <input type="file" onChange={(e) => setVideo(e.target?.files?.item(0)!)} />
+
+
+  </div>) : (<p>Loading...</p >);
 }
 
 export default App;
